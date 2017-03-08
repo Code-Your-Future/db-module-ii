a92b6217d81ef8fa3dad3ad52cb9fb19569fd56f
Angela <Baby007mix@gmail.com>
2017-03-08 21:01:48 +0000

    Test 1 passed!

--- src/routes/organisations.js
+++ src/routes/organisations.js
@@ -4,7 +4,46 @@ const db = require('sqlite');
 
 module.exports = {
     get(req, res) {
-        res.status(404).json({ error: 'Not found' });
+        let organisations;
+        db.all('select * from organisation')
+        .then(function(orgz){
+            organisations = orgz;
+            return Promise.all(orgz.map(function(organisation){
+                return db.all('select Name from Service ' +
+                              'join OrganisationService On Service.Id = Organisationservice.ServiceId ' +
+                              'where OrganisationService.organisationid='+organisation.Id)
+
+            }));
+        })
+        .then(function(servicesOffered){
+            for(var i=0; i<organisations.length; i++){
+                organisations[i].Services=servicesOffered[i];
+            }
+           
+        })
+        .then(function(){
+            return Promise.all(organisations.map(function(organisation){
+                return db.all('select Name,Email from User '+
+                             'join ContactUserOrganisation On User.Id = ContactUserOrganisation.UserId '
+                             +'where ContactUserOrganisation.organisationId ='+organisation.Id)
+            }));
+        })
+        .then(function(userDetails){
+            console.log(userDetails);
+            for(var i=0; i<organisations.length; i++){
+                organisations[i].Contacts=userDetails[i];
+            }
+        })
+        .then(function(){
+         res.json(organisations)   
+     })
+
+        .catch((error)=>{
+            console.log(error)
+            res.status(400).send('You failed to Deliver, Simon Said you must!')
+        })
+
+        
     },
 
     add(req, res) {
